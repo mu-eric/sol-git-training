e0415310eb47ea2077461b505ca98d285b5cd449
Eric <mu.eric@live.ca>
2024-05-02 11:28:13 -0600

    Added asset type function, updated menu

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ while True:
     print('---------------------------')
     print('1) Reverse a string')
     print('2) Check tolerance')
-    print('3) Exit')
+    print('3) Check asset type')
+    print('4) Exit')
     selection = input()
 
     match selection:
@@ -19,10 +20,14 @@ while True:
             try:
                 user_input = float(input('Enter a value: '))
                 result = m.tolerance_check(user_input)
-                print(f'The value ({user_input}) entered is {'' if result else 'not'} within tolerance.')
+                print(f'The value ({user_input}) entered {'is' if result else 'is not'} within tolerance.')
             except Exception as Ex:
                 print(f'Exception occured: {Ex}, enter a different value.')
         case '3':
+            asset_type = input('Enter a asset type name: ')
+            result = m.asset_type_check(asset_type)
+            print(f'{asset_type} is {'a known' if result else 'not a known'} asset type')
+        case '4':
             break
         case _:
             print(f'Invalid selection: {selection}')
